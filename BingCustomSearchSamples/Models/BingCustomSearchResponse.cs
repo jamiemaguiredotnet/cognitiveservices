02f52bb5692bd578140626eeeaff8a13257da5a7
jamiemaguiredotnet <admin@jamiemaguire.net>
2018-12-04 17:08:36 +0000

    1-bing-seach-CustomSearch

---
+++ BingCustomSearchSamples/Models/BingCustomSearchResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingSearchSamples.CustomSearch.Models
+{
+    public class BingCustomSearchResponse
+    {
+        public string _type { get; set; }
+        public WebPages webPages { get; set; }
+    }
+
+    public class WebPages
+    {
+        public string webSearchUrl { get; set; }
+        public int totalEstimatedMatches { get; set; }
+        public WebPage[] value { get; set; }
+    }
+
+    public class WebPage
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string displayUrl { get; set; }
+        public string snippet { get; set; }
+        public DateTime dateLastCrawled { get; set; }
+        public string cachedPageUrl { get; set; }
+        public OpenGraphImage openGraphImage { get; set; }
+    }
+
+    public class OpenGraphImage
+    {
+        public string contentUrl { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}
---
+++ BingCustomSearchSamples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+using BingSearchSamples.CustomSearch.Models;
+
+namespace BingSearchSamples.CustomSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriptionKey = "YOUR-SUBSCRIPTION-KEY";
+            var customConfigId = "YOUR-CUSTOM-CONFIG-ID";
+
+            var searchTerm = "Twitter";
+
+            var url = "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?" + "q=" + searchTerm + "&customconfig=" + customConfigId;
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            var httpResponseMessage = client.GetAsync(url).Result;
+            var responseContent = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            BingCustomSearchResponse response = JsonConvert.DeserializeObject<BingCustomSearchResponse>(responseContent);
+
+            for (int i = 0; i < response.webPages.value.Length; i++)
+            {
+                var webPage = response.webPages.value[i];
+
+                Console.WriteLine("name: " + webPage.name);
+                Console.WriteLine("url: " + webPage.url);
+                Console.WriteLine("displayUrl: " + webPage.displayUrl);
+                Console.WriteLine("snippet: " + webPage.snippet);
+                Console.WriteLine("dateLastCrawled: " + webPage.dateLastCrawled);
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
+
