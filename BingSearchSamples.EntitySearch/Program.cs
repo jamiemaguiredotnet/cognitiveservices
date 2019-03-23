818c5c5931b24e54421457c708c92cd614be2a89
jamie maguire <jamie_maguire@hotmail.com>
2019-03-23 05:31:02 +0000

    added Bing Entity Search

---
+++ BingSearchSamples.EntitySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingSearchSamples.EntitySearch
+{
+    class Program
+    {
+        static string host = "https://api.cognitive.microsoft.com";
+        static string path = "/bing/v7.0/entities";
+        static string market = "en-US";
+        static string key = "";
+
+        static string query = "i  might buy a new iphone in the Verizon store in New York";
+
+        async static void Search()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            string uri = 
+                host + path + "?mkt=" + market + "&q=" + System.Net.WebUtility.UrlEncode(query);
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            string contentString = await response.Content.ReadAsStringAsync();
+            dynamic parsedJson = JsonConvert.DeserializeObject(contentString);
+            Console.WriteLine(parsedJson);
+        }
+
+        static void Main(string[] args)
+        {
+            Search();
+            Console.ReadLine();
+        }
+
+       
+    }
+}
